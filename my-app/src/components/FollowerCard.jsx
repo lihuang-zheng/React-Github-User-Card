e4d82d92b25ad9c1da95287a78103dd06fed3706
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-11 16:52:53 -0600

    prototype material ui for follower card.

--- my-app/src/components/FollowerCard.jsx
+++ my-app/src/components/FollowerCard.jsx
@@ -1,13 +1,60 @@
 import React from "react";
 
+// import material ui
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Hidden from "@material-ui/core/Hidden";
+
+// Card styling for flex
+const useStyles = makeStyles({
+  card: {
+    display: "flex",
+    padding: "10px",
+    margin: "20px 0"
+  },
+  cardDetails: {
+    flex: 1
+  },
+  cardMedia: {
+    width: 200,
+    height: 200
+  }
+});
+
+// below are the function for the follower card
 function FollowerCard(props) {
+  // set useStyles to classes for easier modify
+  const classes = useStyles();
+
   return (
-    <div className="follower-style">
+    <div>
       {props.followers.map(follower => (
-        <div key={follower.id}>
-          <img width="200 " src={follower.avatar_url} alt="profile" />
-          <h2>{follower.login}</h2>
-          <a href={follower.html_url}>Follower Info</a>
+        <div key={follower.id} className={classes.cardBody}>
+          <Grid item xs={12} md={6}>
+            <CardActionArea component="a" href={follower.html_url}>
+              <Card className={classes.card}>
+                <div className={classes.cardDetails}>
+                  <CardContent>
+                    <Typography component="h2" variant="h5">
+                      {follower.login}
+                    </Typography>
+                  </CardContent>
+                </div>
+
+                <Hidden xsDown>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={follower.avatar_url}
+                  />
+                </Hidden>
+              </Card>
+            </CardActionArea>
+          </Grid>
         </div>
       ))}
     </div>
