282e91db5b58d870b9e7d2778ceb619d06f7a1ed
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-11 14:41:08 -0600

    setup the base structure of the Github User.

--- my-app/src/components/GithubUser.jsx
+++ my-app/src/components/GithubUser.jsx
@@ -3,6 +3,40 @@ import React from "react";
 // import axios to grab data from api
 import axios from "axios";
 
-class GithubUser extends React.Component {}
+class GithubUser extends React.Component {
+  //use state for constructor
+  state = {
+    users: [],
+    userText: "",
+    error: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>My Github.</h1>
+
+        <input
+          type="text"
+          value={this.state.userText}
+          onChange={this.handleChanges}
+        />
+
+        <button onClick={this.fetchUsers}>Fetch Users</button>
+
+        {this.state.error && (
+          <p style={{ color: "crimson" }}>{this.state.error}</p>
+        )}
+
+        <div className="users">
+          {!this.state.error &&
+            this.state.users.map(user => (
+              <img width="150" src={user} key={user} alt={user} />
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default GithubUser;
