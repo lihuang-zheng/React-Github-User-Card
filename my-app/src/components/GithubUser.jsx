540e0a41e12e673ba53c13d2d6e0a5b4944a8378
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-11 16:02:59 -0600

    modified the github.

--- my-app/src/components/GithubUser.jsx
+++ my-app/src/components/GithubUser.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+//import cards to github user component
+import FollowerCard from "./FollowerCard";
+import UserCard from "./UserCard";
+
 // import axios to grab data from api
 import axios from "axios";
 
@@ -12,8 +16,9 @@ class GithubUser extends React.Component {
 
   // componentDidMount to get the axios to get data from github api
   componentDidMount() {
+    //user axios
     axios
-      .get("https://api.github.com/users/lihuang-zheng/followers")
+      .get("https://api.github.com/users/lihuang-zheng/")
       .then(res => {
         console.log(res);
 
@@ -22,18 +27,38 @@ class GithubUser extends React.Component {
         });
       })
       .catch(err => console.log(err));
+
+    //user's follower axios
+    axios
+      .get("https://api.github.com/users/lihuang-zheng/followers")
+      .then(res => {
+        console.log(res);
+
+        this.setState({
+          followers: res.data
+        });
+      })
+      .catch(err => console.log(err));
   }
 
+  // render user and user's follower on the page.
   render() {
     return (
-      <div>
-        <h1>My Github.</h1>
+      // <div>
+      //   <h1>My Github.</h1>
 
-        <div className="users">
-          {this.state.users.map(user => (
-            // <img width="150" src={user} key={user} alt={user} />
-            <h2>{this.state.login}</h2>
-          ))}
+      //   <div className="users">
+
+      //   </div>
+      // </div>
+      <div>
+        <header className="Git-header">
+          <h1>My Github.</h1>
+        </header>
+
+        <div className="container">
+          {/* <UserCard user={this.state.user} /> */}
+          <FollowerCard followers={this.state.followers} />
         </div>
       </div>
     );
